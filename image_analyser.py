d0ddb9fe661f11a7eaf363645a74c449b26a614e
sakshi srivastava <sakshisrivastava413@gmail.com>
2018-10-04 21:57:37 +0530

    analyse image by converting it into greyscale and focussing on the face

--- image_analyser.py
+++ image_analyser.py
@@ -12,3 +12,20 @@ face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.x
 emotion_classifier = load_model(emotion_model_path)
 
 emotion_target_size = emotion_classifier.input_shape[1:3]
+
+def analyse_image(image_path):
+
+  print(image_path)
+
+  image = cv2.imread(image_path)
+
+  gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+  faces = face_cascade.detectMultiScale(
+    gray_image,
+    scaleFactor=1.3,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+  )
--- server.py
+++ server.py
@@ -1,8 +1,20 @@
 from flask import request, Flask
 import json
 from utils.image_conversion import create_image_from_base64
+from image_analyser import analyse_image
+
 
 app = Flask(__name__)
+
+@app.route('/analyse_image', methods=['GET', 'POST'])
+def analyse_image_route():
+  data = request.get_json(force=True)
+  if data is None:
+    return "No data"
+  create_image_from_base64(data['image'], './images/uploaded_img.png')
+  res = analyse_image('./images/uploaded_img.png')
+  return res
+
 
 print('Server ready to receive requests')
 
